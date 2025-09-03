64f9be36c61a28997806d6e5eb0dd9c797e30792
gpoinath235-jpg <gpoinath235@gmail.com>
2025-09-03 11:55:15 +0530

    Hi

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mobile menu toggle
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
